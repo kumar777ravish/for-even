5d4fcb50ebacd1b75e3dd88c505494be2bb15764
Ravish Kumar <ravishcistb@gmail.com>
2022-06-28 20:05:20 +0530

    learning c#

---
+++ foreven/foreven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            //Console.WriteLine("print all even numbers between 100 to 500");
+            for (i = 100; i <= 500; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    Console.WriteLine("{0} is divisible by 2", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not divisible by 2", i);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
